721c7e675a26927718396c33803d199b713ee597
Mineny <minenyaraya@gmail.com>
2023-04-22 11:33:05 -0500

    week1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine(); 
+        Console.Write("What is your lastname? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {lastname} , {name} {lastname}");
     }
 }
