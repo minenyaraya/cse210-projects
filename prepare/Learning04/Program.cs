5e7b6f04192fe01d56b71479a426a1075e24f40b
Mineny <minenyaraya@gmail.com>
2023-05-31 22:02:13 -0500

    Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Mineny Araya", "Multiplicar");
+        Console.WriteLine(a1.GetSumary());
+
+        MathAssignment a2 = new MathAssignment("MiLy Sutter", "Fraction", "7.3", "8-19");
+        Console.WriteLine(a2.GetSumary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Gordo", "European History", "The causes of world War II");
+        Console.WriteLine(a3.GetSumary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
